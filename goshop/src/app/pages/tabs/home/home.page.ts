30890296fbf7f68e725ef45b7a726e79bd2cb670
OusmanaTraore <traoreosman@yahoo.fr>
2022-06-11 16:07:28 +0200

    ajout de  bannière et liste

--- goshop/src/app/pages/tabs/home/home.page.ts
+++ goshop/src/app/pages/tabs/home/home.page.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  banners: any[] = [];
+  restaurants: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.banners = [
+      {banner:'assets/imgs/avatar-ben.png'},
+      {banner:'assets/imgs/avatar-finn.png'},
+      {banner:'assets/imgs/avatar-han.png'},
+    ];
+    this.restaurants= [
+      {
+        cover: 'assets/imgs/avatar-ben.png',
+        name: 'Stayfit',
+        cuisines:[
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 10
+      },
+      {
+        cover: 'assets/imgs/avatar-han.png',
+        name: 'Stayfit',
+        cuisines:[
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 10
+      },
+      {
+        cover: 'assets/imgs/avatar-finn.png',
+        name: 'Stayfit3',
+        cuisines:[
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 10
+      },
+
+    ];
   }
 
 }
